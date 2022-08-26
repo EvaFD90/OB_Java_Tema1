0c0e56169f1cca8781f92fa5a0470a99b376a752
evafi <evafilgueirasaxo@gmail.com>
2022-08-26 18:01:13 +0200

    init commit

---
+++ src/com/example/DataTypes.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Numeric type
+        byte number1 = 1;
+        System.out.println(number1);
+        short number2 = 19;
+        System.out.println(number2);
+        int number3 = 45;
+        System.out.println(number3);
+        long number4 = 100;
+        System.out.println(number4);
+
+        float decimal1 = 5.5f;
+        System.out.println(decimal1);
+        double decimal2 = 10.4d;
+        System.out.println(decimal2);
+
+        // Boolean type
+        boolean variable = false;
+        System.out.println(variable);
+        boolean variable1 = true;
+        System.out.println(variable1);
+
+        // Text type
+        char text1 = 'a';
+        System.out.println(text1);
+        String text2 = "Example of String";
+        System.out.println(text2);
+    }
+}
